b509f7e5e98ba8901c0d504c6443a511483b72b2
remco <remco@remcode.net>
2023-07-11 16:27:54 +0200

    Update routes and configuration for /api subpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
-  resources :replies, except: [:index]
-  resources :locations, except: [:new, :edit]
-  resources :keywords, except: [:new, :edit]
-  resources :post_keywords, except: [:new, :edit]
+  
+  scope '/api' do
+    resources :users
+    resources :posts
+    resources :replies, except: [:index]
+    resources :locations, except: [:new, :edit]
+    resources :keywords, except: [:new, :edit]
+    resources :post_keywords, except: [:new, :edit]
 
+  end
 end
