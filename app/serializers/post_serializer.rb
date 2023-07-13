2cd6bc7f1f17af7e0add4b6a5e39a86a8e5ef458
remco <remco@remcode.net>
2023-07-13 07:08:56 +0200

    fixed serializer polymorphic

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,7 +2,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :content
 
-  belongs_to :user
+  belongs_to :postable, polymorphic: true
   belongs_to :location
   has_many :replies
   has_many :keywords
